526b1306399f55a253de2044cf57387c8bac05fa
FIZLIN <akostov94@gmail.com>
2020-01-22 13:21:27 +0200

    Minor tweak;

--- src-server/index.js
+++ src-server/index.js
@@ -13,6 +13,7 @@ const bodyParser = require('body-parser');
 
 io.on('connection', function (socket) {
   console.log('a user connected');
+  setTimeout(() => io.send('sample message'), 1000);
   socket.on('disconnect', function () {
     console.log('user disconnected');
   });
