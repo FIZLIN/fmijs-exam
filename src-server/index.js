ec46cb700d88f2307e5db40281df42ce19eaa932
FIZLIN <akostov94@gmail.com>
2020-01-22 13:28:50 +0200

    lint;

--- src-server/index.js
+++ src-server/index.js
@@ -1,10 +1,6 @@
 const express = require('express');
-const path = require('path');
 const cors = require('cors');
-const ps = require('ps-node');
 const psList = require('ps-list');
-
-
 
 const app = express();
 const server = require('http').Server(app);
